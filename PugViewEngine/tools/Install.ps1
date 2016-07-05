d9d6bbadae7092dd1a66fbca153f648d7c7678bb
Pat Lapinski <entith@entith.net>
2016-07-05 13:40:40 -0400

    Fixed NuGet install.

---
+++ PugViewEngine/tools/Install.ps1
@@ -0,0 +1,23 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+function MarkDirectoryAsCopyToOutputRecursive($item)
+{
+    $item.ProjectItems | ForEach-Object { MarkFileASCopyToOutputDirectory($_) }
+}
+
+function MarkFileASCopyToOutputDirectory($item)
+{
+    Try
+    {
+        Write-Host Try set $item.Name
+        $item.Properties.Item("CopyToOutputDirectory").Value = 2
+    }
+    Catch
+    {
+        Write-Host RecurseOn $item.Name
+        MarkDirectoryAsCopyToOutputRecursive($item)
+    }
+}
+
+MarkDirectoryAsCopyToOutputRecursive($project.ProjectItems.Item("node_modules"))
+$project.ProjectItems.Item("pugViewEngine.js").Properties.Item("CopyToOutputDirectory").Value = 2
